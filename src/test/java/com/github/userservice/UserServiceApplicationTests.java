39a5cde370d4de614e11c91891116e96d91597dc
spark <fabricio.jacob@outlook.com>
2023-06-30 08:18:25 -0300

    init project

--- src/test/java/com/github/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/github/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,37 @@
 package com.github.userservice;
 
+import com.github.userservice.dto.TransactionRequest;
+import com.github.userservice.dto.TransactionResponse;
+import com.github.userservice.dto.TransactionType;
+import io.rsocket.transport.netty.client.TcpClientTransport;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+	@Autowired
+	private RSocketRequester.Builder builder;
+
 	@Test
-	void contextLoads() {
+	void transactionTest() {
+		var request = builder.transport(TcpClientTransport.create("localhost", 7073));
+
+		var result = request.route("user.transaction")
+				.data(TransactionRequest.builder()
+						.userId("001")
+						.amount(10)
+						.type(TransactionType.CREDIT)
+						.build()).retrieveMono(TransactionResponse.class)
+				.doOnNext(System.out::println);
+
+		StepVerifier.create(result)
+				.expectNextCount(1)
+				.verifyComplete();
+
 	}
 
 }
